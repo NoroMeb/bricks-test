cbb59841d85fdcab51bffac11968ccd9caacb35a
Noro <noromeb85@gmail.com>
2023-03-20 16:43:35 +0100

    added transferOriginal function to retriev Original NFT

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
-contract Vault is ERC721Holder {}
+contract Vault is ERC721Holder {
+    function transferOriginal(address contractAddress, uint256 tokenId, address receiver) external {
+        IERC721(contractAddress).transferFrom(address(this), receiver, tokenId);
+    }
+}
